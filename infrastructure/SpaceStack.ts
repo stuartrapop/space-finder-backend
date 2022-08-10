bb866fdb8d21e6d385a4be31a016cd06a8ce08c2
Stuart Rapoport <srapoport@dial-once.com>
2022-08-10 14:28:25 +0200

    first commit

---
+++ infrastructure/SpaceStack.ts
@@ -0,0 +1,36 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+import { join } from 'path';
+import { GenericTable } from './GenericTables';
+
+export class SpaceStack extends Stack {
+  private api = new RestApi(this, 'SpaceApi');
+  private spacesTable = new GenericTable('SpacesTable', 'spaceId', this);
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main',
+    });
+
+    const helloLamdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
+      handler: 'handler',
+      functionName: 'helloLambdaNodeJs',
+    });
+
+    // Hello Api lambda integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('Get', helloLambdaIntegration);
+  }
+}
